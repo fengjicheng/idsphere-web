269e8767895e51dc06dd53c5f07d5eb41be39f9d
yuyan <yuyan@50yc.com>
2025-03-31 15:59:24 +0800

    update：钉钉、企业企业和飞书扫码JS文件支持动态引入

--- src/main.js
+++ src/main.js
@@ -26,6 +26,30 @@ Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
+// 加载第三方SDK
+function loadExternalScript(url) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script')
+    script.src = url
+    script.onload = resolve
+    script.onerror = reject
+    document.body.appendChild(script)
+  })
+}
+(async function loadAuthSDKs() {
+  if (process.env.VUE_APP_DINGTALK_CLIENT_ID) {
+    await loadExternalScript('https://g.alicdn.com/dingding/h5-dingtalk-login/0.21.0/ddlogin.js')
+  }
+
+  if (process.env.VUE_APP_WECHAT_APP_ID && process.env.VUE_APP_WECHAT_AGENT_ID) {
+    await loadExternalScript('https://wwcdn.weixin.qq.com/node/wework/wwopen/js/wwLogin-1.2.7.js')
+  }
+
+  if (process.env.VUE_APP_FEISHU_CLIENT_ID) {
+    await loadExternalScript('https://lf-package-cn.feishucdn.com/obj/feishu-static/lark/passport/qrcode/LarkSSOSDKWebQRCode-1.0.3.js')
+  }
+})()
+
 new Vue({
   el: '#app',
   router,
